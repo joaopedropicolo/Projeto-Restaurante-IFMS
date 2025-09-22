0a4d3cfc964371e7d2264688af093fb85f59aa7e
joaopedropicolo <bigwon132@gmail.com>
2025-09-22 14:06:19 -0400

    Cadastro Feito

---
+++ prisma/migrations/20250922180548_change_senha_to_password/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `senha` on the `Usuario` table. All the data in the column will be lost.
+  - Added the required column `password` to the `Usuario` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `tipo` to the `Usuario` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Usuario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL
+);
+INSERT INTO "new_Usuario" ("email", "id", "nome") SELECT "email", "id", "nome" FROM "Usuario";
+DROP TABLE "Usuario";
+ALTER TABLE "new_Usuario" RENAME TO "Usuario";
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
